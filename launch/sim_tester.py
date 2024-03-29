bde8da81069e700e67b1a33184aa1c74339e6ef3
Brandon Sutherland <brandonsutherland2@gmail.com>
2024-03-29 12:13:50 -0600

    added estimated and true pose to sim tester

--- launch/sim_tester.py
+++ launch/sim_tester.py
@@ -16,8 +16,9 @@ field_range = np.array([[0, 10], [0, 10]])
 
 landmarks = generate_uniform_random_landmarks(15, field_range)
 path = arc_path(50, np.array([0, 0]), np.array([10, 10]), 30)
+noisy_path = path + np.random.normal(0, 0.05, path.shape)
 measurements, measurement_associations = generate_gaussian_measurements(path, landmarks, range_std=0.05, bearing_std=0.05, max_range=4)
 
-plot_field(landmarks=landmarks, estimated_poses=path,
+plot_field(landmarks=landmarks, estimated_poses=noisy_path, true_poses=path,
            measurement_associations=measurement_associations)
 
