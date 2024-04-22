f4631b553c52bc18835904374f1180e5ab85dd91
Brandon Sutherland <brandonsutherland2@gmail.com>
2024-04-22 11:07:34 -0600

    added bearing measurements to estimate

--- launch/chapter_2.py
+++ launch/chapter_2.py
@@ -28,12 +28,12 @@ np.random.seed(11)
 
 
 # Noise/bias parameters
-measurement_range_std = 0.1
-measurement_bearing_std = 0.05
-odometry_range_bias = 0.05
-odometry_range_std = 0.05
-odometry_angle_bias = 0.05
-odometry_angle_std = 0.05
+measurement_range_std = 0.1     # m
+measurement_bearing_std = 0.05  # rad
+odometry_range_bias = 0.05      # m
+odometry_range_std = 0.05       # m
+odometry_angle_bias = 0.05      # rad
+odometry_angle_std = 0.05       # rad
 
 
 # Generate simulated information for solver
@@ -53,20 +53,27 @@ odometry, odometry_path = generate_odometry(path, range_std=odometry_range_std,
 measurement_poses = odometry_path[measurement_associations[:, 0]]
 measurement_landmarks = landmarks[measurement_associations[:, 1]]
 J_ranges = range_to_location_jacobian(measurement_poses, measurement_landmarks)
-A = np.zeros((measurements.shape[0], odometry_path.shape[0] * (odometry_path.shape[1] - 1)))
-for i, j in enumerate(measurement_associations[:, 0]):
-    pose_idx = j * 2
-    A[i, pose_idx:pose_idx + 2] = J_ranges[i, 0, :2]
-A = A / measurement_range_std
-b = (measurements[:, 0] - range_to_location(measurement_poses, measurement_landmarks)).reshape(-1, 1) / measurement_range_std
+J_bearings = bearing_to_location_jacobian(measurement_poses, measurement_landmarks)
 
+pose_length = odometry_path.shape[1]
+A = np.zeros((measurements.shape[0]*2, odometry_path.shape[0] * pose_length))
+for i, j in enumerate(measurement_associations[:, 0]):
+    pose_idx = j * pose_length
+    A[i, pose_idx:pose_idx + pose_length] = J_ranges[i, 0] / measurement_range_std
+    A[i + measurements.shape[0], pose_idx:pose_idx + pose_length] = \
+            J_bearings[i, 0] / measurement_bearing_std
+
+b_ranges = (measurements[:, 0] - range_to_location(measurement_poses, measurement_landmarks)).reshape(-1, 1) / measurement_range_std
+b_bearings = (measurements[:, 1] - bearing_to_location(measurement_poses, measurement_landmarks)).reshape(-1, 1) / measurement_bearing_std
+
+b = np.vstack((b_ranges, b_bearings))
 
 # Solve the least squares problem
-delta = np.linalg.solve(A.T @ A, A.T @ b).reshape(-1, 2)
-x = odometry_path[:, :2] + delta
+delta = np.linalg.solve(A.T @ A, A.T @ b).reshape(-1, pose_length)
+x = odometry_path + delta
 
 
 # Plot the results
-plot_field(landmarks=landmarks, true_poses=path, estimated_poses=x,
+plot_field(landmarks=landmarks, true_poses=path, estimated_poses=x[:, :2],
            measurement_associations=measurement_associations)
 
